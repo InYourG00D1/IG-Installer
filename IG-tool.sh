7bf3bb769fe17617f2b845a284270985edcce98d
Ragil}{P <44290386+InYourG00D1@users.noreply.github.com>
2020-02-29 19:57:31 +0700

    Add files via upload

---
+++ IG-tool.sh
@@ -0,0 +1,167 @@
+#!/system/xbin/bash
+
+blue='\033[34;1m'
+green='\033[32;1m'      
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'             
+yellow='\033[33;1m'
+
+clear
+echo "\033[34;1m]
+ _
+ 0  /***
+ | /
+ | \   _
+ |__\___| Installer_______________________
++ coded by : InYourG00D                   +
++ Team : SoulTeamCyber                    +
++_GitHub : https://github.com/InYourG00D1_+
+    ^...^             ^...^
+   <_* *_> Instagram <_* *_> 
+     \_/               \_/  
+$red "[!] If your account has been hit by a logger, report me immediately "
+$red "[!] Not all tools can be run on Termux"
+"
+echo $white"|________Function_________________|_Active_|"
+echo $white"| 1  |" dwonstgram $green "                  [ON]"
+echo $white"| 2  |" followers scraper $green "           [ON]"
+echo $white"| 3  |" tag search $green "                  [ON]"
+echo $white"| 4  |" instabot $green "                    [ON]"
+echo $white"| 5  |" VideoThumbnailSelectionView $green " [ON]"
+echo $white"| 6  |" photo finder $green "                [ON]"
+echo $white"| 7  |" grabber $green "                     [ON]"
+echo $white"| 8  |" account checker $green "             [ON]"
+echo $white"| 9  |" report $green "                      [ON]"
+echo $white"| 10 |" post download $green "               [ON]"
+echo $white"| 11 |" crawler $green "                     [ON]"
+echo $white"| 12 |" PHP scraper $green "                 [ON]"
+echo $white"| 13 |" photo picker $green "                [ON]"
+echo $white"| 14 |" filter $green "                      [ON]"
+echo $white"| 15 |" LikeColorTransitionAndroid $green "  [ON]"
+echo $white"| 00 |" exit $red "                        [OFF]"
+read -p ">" ragil
+
+if [ $ragil = 1 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/rogeriopvl/downstagram
+fi 
+
+
+if [ $ragil = 2 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/tonoli/instagram-followers-scraper
+fi 
+
+if [ $ragil = 3 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/UCSDTT/tag-search
+fi 
+
+
+if [ $ragil = 4 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/lukschwalb/Instabot
+fi 
+
+
+if [ $ragil = 5 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/sarpsolakoglu/VideoThumbnailSelectionView
+fi 
+
+
+if [ $ragil = 6 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/nprapps/photo-finder
+fi 
+
+
+if [ $ragil = 7 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/Aspoky/instagram_grabber
+fi 
+
+if [ $ragil = 8 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/Fr13ND3/user-checker-instagram
+fi 
+
+
+if [ $ragil = 9 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/Aspoky/report-tool
+fi 
+
+
+if [ $ragil = 10 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/Vurku/instagram-post-downloader
+fi 
+
+
+if [ $ragil = 11 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/tzuhsial/InstagramCrawler
+fi 
+
+
+if [ $ragil = 12 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/postaddictme/instagram-php-scraper
+fi 
+
+if [ $ragil = 13 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/wenzhaot/InstagramPhotoPicker
+fi 
+
+
+if [ $ragil = 14 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/yulu/Instagram_Filter
+fi 
+
+
+if [ $ragil = 15 ]
+then 
+clear 
+apt update && apt upgrade
+git clone https://github.com/Taishi-Y/InstagramLikeColorTransitionAndroid
+fi 
+
+if [ $ragil = 00 ]
+then
+clear
+echo $white "Kontak Me : 081413512754"
+echo $white "Use Wisely"
+fi
